86fa0915a66fdba50c1a15aa84d2ef220485b0b7
ghassen naouar <ghassennaouar7@gmail.com>
2025-08-05 20:28:48 +0100

    1389A

---
+++ math_basics/1389A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define boost()                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL);
+
+
+void solve() {
+    ll l,r;
+    cin >> l >> r;
+    if(2*l <= r){
+        cout << l <<" "<< 2*l << endl;
+    }
+    else{
+        cout << -1 <<" "<< -1 << endl;
+    }
+
+  
+}
+
+int main() {
+  boost();
+
+  ll t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
